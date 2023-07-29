4c8761ef92328c05090720dff880e322a508a12e
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-07-29 18:40:04 +0200

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿partial class MainForm
+{
+    private System.ComponentModel.IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.Animated = true;
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox1.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(195)))), ((int)(((byte)(201)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(769, 9);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 0;
+            // 
+            // guna2ControlBox2
+            // 
+            this.guna2ControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox2.Animated = true;
+            this.guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.guna2ControlBox2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2ControlBox2.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(195)))), ((int)(((byte)(201)))));
+            this.guna2ControlBox2.IconColor = System.Drawing.Color.Black;
+            this.guna2ControlBox2.Location = new System.Drawing.Point(718, 9);
+            this.guna2ControlBox2.Name = "guna2ControlBox2";
+            this.guna2ControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(356, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 54);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "03:00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(258, 216);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(308, 290);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            // 
+            // MainForm
+            // 
+            this.AccentColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(219)))), ((int)(((byte)(223)))));
+            this.AllowResize = false;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(823, 529);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ControlBox2);
+            this.Controls.Add(this.guna2ControlBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.State = MetroSuite.MetroForm.FormState.Custom;
+            this.Style = MetroSuite.Design.Style.Light;
+            this.Text = "Contatore coccole";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+    private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Timer timer1;
+}
---
+++ MainForm.cs
@@ -0,0 +1,124 @@
+﻿using MetroSuite;
+using NAudio.Wave;
+using System;
+using System.IO;
+using System.Threading;
+
+public partial class MainForm : MetroForm
+{
+    private int _currentSeconds;
+    private ProtoRandom.ProtoRandom _random;
+    private int _randomSeconds;
+
+    public MainForm()
+    {
+        InitializeComponent();
+        _random = new ProtoRandom.ProtoRandom(5);
+        new Thread(UpdateTimer).Start();
+        CheckForIllegalCrossThreadCalls = false;
+        new Thread(SetRandomSeconds).Start();
+        timer1.Start();
+    }
+
+    public void PlayStartSound()
+    {
+        PlaySound("sounds\\start.mp3");
+    }
+
+    public void PlayEndSound()
+    {
+        PlaySound("sounds\\end.mp3");
+    }
+
+    public void PlaySound(string fileName)
+    {
+        Mp3FileReader reader = new Mp3FileReader(fileName);
+        WaveOutEvent waveOut = new WaveOutEvent();
+        waveOut.Init(reader);
+        waveOut.Play();
+    }
+
+    public void SetRandomSeconds()
+    {
+        _randomSeconds = _random.GetRandomInt32(int.Parse(File.ReadAllText("configs\\min_seconds_limit.txt")), int.Parse(File.ReadAllText("configs\\max_seconds_limit.txt")));
+
+        while (true)
+        {
+            Thread.Sleep(1000);
+
+            if (_randomSeconds == 0)
+            {
+                break;
+            }
+
+            _randomSeconds--;
+        }
+
+        PlayStartSound();
+        new Thread(StartTimer).Start();
+    }
+
+    public void StartTimer()
+    {
+        _currentSeconds = int.Parse(File.ReadAllText("configs\\seconds_config.txt"));
+
+        while (true)
+        {
+            Thread.Sleep(1000);
+
+            if (_currentSeconds == 0)
+            {
+                break;
+            }
+
+            _currentSeconds--;
+        }
+
+        PlayEndSound();
+        new Thread(SetRandomSeconds).Start();
+    }
+
+    public void UpdateTimer()
+    {
+        while (true)
+        {
+            int nCurrentHours = 0, nCurrentMinutes = 0, nTheSeconds = _currentSeconds;
+
+            while (nTheSeconds >= 3600)
+            {
+                nTheSeconds -= 3600;
+                nCurrentHours++;
+            }
+
+            while (nTheSeconds >= 60)
+            {
+                nTheSeconds -= 60;
+                nCurrentMinutes++;
+            }
+
+            string currentHours = nCurrentHours.ToString(), currentMinutes = nCurrentMinutes.ToString(), currentSeconds = nTheSeconds.ToString();
+
+            if (currentHours.Length == 1)
+            {
+                currentHours = "0" + currentHours;
+            }
+
+            if (currentMinutes.Length == 1)
+            {
+                currentMinutes = "0" + currentMinutes;
+            }
+
+            if (currentSeconds.Length == 1)
+            {
+                currentSeconds = "0" + currentSeconds;
+            }
+
+            label1.Text = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+        }
+    }
+
+    private void MainForm_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+    {
+        Environment.Exit(0);
+    }
+}
